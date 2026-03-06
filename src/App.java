98f9e3e381f299eeb526215c4e0681a511c572ab
Isaac Lima Simonetti <isaac.limasimonetti@gmail.com>
2026-03-06 17:58:56 -0300

    Aula 4
    Colocando fundo e o pássaro

--- src/App.java
+++ src/App.java
@@ -6,12 +6,15 @@ public class App {
         int LarguraBorda = 360;
         int AlturaBorda = 640;
 
-        JFrame janela = new JFrame("Flappy Bird");
-        janela.setVisible(true);
+        JFrame janela = new JFrame("Flappy Bird");       
         janela.setSize(LarguraBorda, AlturaBorda);
         janela.setLocationRelativeTo(null);
         janela.setResizable(false);
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        FlappyBird flappyBird = new FlappyBird();
+        janela.add(flappyBird);
+        janela.pack();
+        janela.setVisible(true);
     }
 }
--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -11,8 +11,59 @@ public class FlappyBird extends JPanel{
     int AlturaBorda = 640;
 
 
+    //IMAGEM
+
+    Image birdImage;
+    Image backgroundImage;
+    Image bottomPipeImage;
+    Image topPipImage;
+
+
+    //PASSARO
+
+    int birdX = LarguraBorda / 8;
+    int birdY = AlturaBorda / 2;
+    int birdWidth = 34;
+    int birdHeight = 24;
+
+    class Bird{
+        int x = birdX;
+        int y = birdY;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+
+
+        Bird(Image img) {
+            this.img = img;
+        }
+    }
+
+    //LOGICA DO JOGO
+    Bird bird;
+
+
+
     FlappyBird(){
         setPreferredSize(new Dimension(LarguraBorda, AlturaBorda));
-        setBackground(Color.blue);
+
+        backgroundImage = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImage = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipImage = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImage = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+
+        bird = new Bird(birdImage);
     }
+
+    public void paintComponent (Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw (Graphics g){
+        g.drawImage(backgroundImage, 0, 0, LarguraBorda, AlturaBorda, null);
+
+        g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+    }
+
 }
